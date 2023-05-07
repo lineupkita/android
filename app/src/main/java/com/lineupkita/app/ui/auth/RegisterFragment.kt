bd43f8f3a1fba9db5d2c7e639a5d009590dd8702
arysugiarto <arysugiarto10@gmail.com>
2023-05-07 14:24:27 +0700

    add screen register

--- app/src/main/java/com/lineupkita/app/ui/auth/RegisterFragment.kt
+++ app/src/main/java/com/lineupkita/app/ui/auth/RegisterFragment.kt
@@ -1,18 +1,18 @@
-package com.lineupkita.app.ui.login
+package com.lineupkita.app.ui.auth
 
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.lineupkita.app.R
-import com.lineupkita.app.databinding.FragmentLoginBinding
+import com.lineupkita.app.databinding.FragmentRegisterBinding
 //import com.arysugiarto.attendence.ui.main.MainFragment.Companion.parentBottomAppBar
 import com.lineupkita.app.util.*
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class LoginFragment : Fragment(R.layout.fragment_login) {
+class RegisterFragment : Fragment(R.layout.fragment_register) {
 
-    private val binding by viewBinding<FragmentLoginBinding>()
+    private val binding by viewBinding<FragmentRegisterBinding>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -42,7 +42,7 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
 
     private fun initOnClick() {
         binding.apply {
-
+            btnRegister.setOnClickListener(onClickCallback)
         }
     }
 
@@ -50,7 +50,12 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
 
     private val onClickCallback = View.OnClickListener { view ->
         when (view) {
+            binding.btnRegister -> {
+                navController.navigateOrNull(
+                    RegisterFragmentDirections.actionRegisterFragmentToMainFragment()
+                )
 
+            }
         }
 
     }
